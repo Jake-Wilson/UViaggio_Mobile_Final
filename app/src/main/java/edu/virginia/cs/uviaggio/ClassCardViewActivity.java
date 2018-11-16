468a7d5fd3ec5a444d82c4fa75bd4e6f9e93514c
Stephen Thiringer <sjt7zn@virginia.edu>
2018-11-15 20:17:51 -0500

    Progress

---
+++ app/src/main/java/edu/virginia/cs/uviaggio/ClassCardViewActivity.java
@@ -0,0 +1,13 @@
+package edu.virginia.cs.uviaggio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassCardViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_card_view);
+    }
+}
