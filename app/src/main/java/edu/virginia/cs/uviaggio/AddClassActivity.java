31c2bb086be1de8d17ccf36c3c90957bcf530367
Jake Wilson <jaw4fz@virginia.edu>
2018-12-04 20:09:31 -0500

    add class string format checking

--- app/src/main/java/edu/virginia/cs/uviaggio/AddClassActivity.java
+++ app/src/main/java/edu/virginia/cs/uviaggio/AddClassActivity.java
@@ -3,8 +3,11 @@ package edu.virginia.cs.uviaggio;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -23,11 +26,15 @@ import org.w3c.dom.Text;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AddClassActivity extends AppCompatActivity {
     public static final String BASE_URL = "http://stardock.cs.virginia.edu/louslist/Courses/view/";
-
+    boolean classText = false;
+    boolean secText = false;
     EditText courseEditText, sectionEditText;
+    Button dl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,54 @@ public class AddClassActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_class);
         courseEditText = (EditText) findViewById(R.id.courseEditText);
         sectionEditText = (EditText) findViewById(R.id.sectionEditText);
+        dl = findViewById(R.id.callWebServiceButton);
+        final String expression = "[A-Z]{2,4} [0-9]{4}";
+        final String expression2 = "[0-9]{3}";
+        courseEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Pattern pattern = Pattern.compile(expression);
+                Matcher matcher = pattern.matcher(s);
+                classText = matcher.matches();
+                if(secText && classText){
+                    Button dl = findViewById(R.id.callWebServiceButton);
+                    dl.setEnabled(true);
+                } else { dl.setEnabled(false);}
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        sectionEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Pattern pattern = Pattern.compile(expression2);
+                Matcher matcher = pattern.matcher(s);
+                secText = matcher.matches();
+                if(secText && classText){
+                    dl.setEnabled(true);
+                }else{ dl.setEnabled(false);}
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 
     public void downloadData(View view) {
